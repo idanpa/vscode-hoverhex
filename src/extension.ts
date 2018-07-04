ed8478c7cb1768340f904317929d8c81bf5c4d48
idanp <idan.kp@gmail.com>
2018-07-04 17:22:14 +0300

    Splitting the hex string to groups of 4

--- src/extension.ts
+++ src/extension.ts
@@ -2,17 +2,25 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-    let aa = vscode.languages.registerHoverProvider({scheme: '*', language: '*'}, { 
+    let aa = vscode.languages.registerHoverProvider({scheme: '*', language: '*'}, {
         provideHover(document, position, token) {
             const hoveredWord = document.getText(document.getWordRangeAtPosition(position));
             if (/^0x[0-9a-fA-F]+$/g.test(hoveredWord)) {
                 var x = parseInt(hoveredWord, 16);
-                return new vscode.Hover(hoveredWord + ' = ' + x);
+                var splitWord = [];
+                var len = hoveredWord.length;
+
+                for(var i = len; i > 6; i -= 4) {
+                    splitWord.push(hoveredWord.substr(i - 4, 4))
+                }
+                splitWord.push(hoveredWord.substr(0, i))
+
+                return new vscode.Hover( splitWord.reverse().join("_") + ' = ' + x);
             }
-            
+
         }
     });
-    context.subscriptions.push(aa); 
+    context.subscriptions.push(aa);
 }
 
 export function deactivate() {
